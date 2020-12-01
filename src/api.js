24cd9dd92e7117fa9257dd09fea77cfa156bf666
Maciej Mondalski <maciej.mondalski@gmail.com>
2020-12-01 19:46:26 +0100

    update

--- src/api.js
+++ src/api.js
@@ -10,11 +10,11 @@ const getCurrentMonth = () => {
     return month;
   }
 };
-
+//Getting the date
 const getCurrentDay = () => {
   const day = new Date().getDate();
   if (day < 10) {
-    return `0${month}`;
+    return `0${day}`;
   } else {
     return day;
   }
@@ -24,15 +24,22 @@ const getCurrentDay = () => {
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
-const currentDate = `${currentDay}-${currentMonth}-${currentYear}`;
-const lastYear = `${currentDay}-${currentMonth}-${currentYear - 1}`;
-const nextYear = `${currentDay}-${currentMonth}-${currentYear + 1}`;
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
-const popular_games = `games?dates=${lastYear}, ${currentDate}&ordering=-rating&page_size=10`;
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=12`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=12`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=12`;
 
-export const popularGamesURL = () => {
-  `${base_url}${popular_games}`;
-};
-
-console.log(pop);
+export const popularGamesURL = () => `${base_url}${popular_games}`;
+export const upcomingGamesURL = () => `${base_url}${upcoming_games}`;
+export const newGamesURL = () => `${base_url}${newGames}`;
+//GAME DETAILS
+export const gameDetailsURL = (game_id) => `${base_url}games/${game_id}`;
+//Game ScreenShots
+export const gameScreenshotURL = (game_id) => `${base_url}games/${game_id}/screenshots`;
+//Searched game
+export const searchGameURL = (game_name) =>
+  `${base_url}games?search=${game_name}&page_size=9`;
